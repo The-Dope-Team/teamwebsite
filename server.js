a3b7897f2156aa9fe779d23903193cb6b13034b3
braunsonY <you18012@byui.edu>
2022-03-15 11:03:19 -0600

    fixed server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = 3000;
 
 const app = express();
 
-app.get('/', (req,res) => {res.send("Hello Browser)});
+app.get('/', (req,res) => {res.send("Hello Browser")});
 
-app.listen(port, ()=>console.log("Listening));
+app.listen(port, ()=>console.log("Listening"));
