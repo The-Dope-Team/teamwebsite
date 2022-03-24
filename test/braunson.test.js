219a71f5615d7edaa20146be01b750688e4c4aae
braunsonY <you18012@byui.edu>
2022-03-24 11:03:40 -0600

    fix

--- test/braunson.test.js
+++ test/braunson.test.js
@@ -1,7 +1,7 @@
 import load_braunson_url from '../utils/load_braunson_url.mjs';
 import assert from assert
 
-it ("Tests Braunson URL Endpoint", async()=>{
+it ("Tests Braunson URL Endpoint", async ()=>{
     const Urlresponse = await load_braunson_url();
     console.log("URL Response:")+Urlresponse;
     assert.equal(Urlresponse, "Hey Braunson, How is life?");
