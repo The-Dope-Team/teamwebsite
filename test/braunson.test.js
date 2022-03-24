5f6f463c268b07475a6108018a9d739194579ba8
braunsonY <you18012@byui.edu>
2022-03-24 11:11:15 -0600

    fixed typo?

--- test/braunson.test.js
+++ test/braunson.test.js
@@ -1,9 +1,9 @@
-import load_braunson_url from '../utils/load_braunson_url.mjs';
+import loadbraunsonurl from '../utils/load_braunson_url.mjs';
 import assert from assert
 
 it ("Tests Braunson URL Endpoint", async ()=>{
-    const urlresponse = await load_braunson_url();
-    console.log("URL Response: ")+urlresponse;
-    assert.equal(urlresponse, "Hello Braunson");
+    const urlResponse = await loadbraunsonurl();
+    console.log("URL Response: ")+urlResponse;
+    assert.equal(urlResponse, "Hello Braunson");
 
 });
