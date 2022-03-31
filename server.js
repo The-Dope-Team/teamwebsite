1ac90fffbdf7b2db09a4996fde8a70b9e3b53709
toddgunn <79170364+toddgunn@users.noreply.github.com>
2022-03-31 10:36:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = 3000;
 const app = express();
 
 app.get('/', (req,res) => {res.send("Hello Browser")});
-app.get('/gunn'), (req,res) => {res.send("Hello Browser")};
 app.get('/braunson', (req,res) => {res.send("Hello Braunson")});
 app.get('/gilber', (req,res) => {res.send("Hello Gilber")});
 app.get('/logan', (req,res) => {res.send("Hello Logan")});
