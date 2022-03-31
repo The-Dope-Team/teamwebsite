600fed8b4b28ccf379c199a5c7dd2371d536a628
toddgunn <todd.k.gunn@gmail.com>
2022-03-31 10:24:46 -0600

    hello

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'; 
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+app.get('/gunn'), (req,res) => {res.send("Hello Browser")};
+app.get('/braunson', (req,res) => {res.send("Hello Braunson")});
+app.get('/gilber', (req,res) => {res.send("Hello Gilber")});
+app.get('/logan', (req,res) => {res.send("Hello Logan")});
+app.listen(port, ()=>console.log("Listening"));
