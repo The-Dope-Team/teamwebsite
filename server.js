1a9b74725bf001eb95a7ae86fe76b8ac4f0433ad
toddgunn <79170364+toddgunn@users.noreply.github.com>
2022-03-31 10:45:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,4 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 app.get('/braunson', (req,res) => {res.send("Hello Braunson")});
 app.get('/gilber', (req,res) => {res.send("Hello Gilber")});
 app.get('/logan', (req,res) => {res.send("Hello Logan")});
+app.listen(port, ()=>console.log("Listening"));
